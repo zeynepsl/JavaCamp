385eddd2990a2f4e460018f266869b211ba2d9a2
zeynepsl <zeyneprsalman@gmail.com>
2021-05-08 19:07:16 +0300

    day:3 homework:2

--- day3Homework2/src/day3Homework2/Instructor.java
+++ day3Homework2/src/day3Homework2/Instructor.java
@@ -1,5 +1,7 @@
 package day3Homework2;
 
 public class Instructor extends User{
-
+	private int id;
+	private String firstName;
+	private String lastName;
 }
--- day3Homework2/src/day3Homework2/Student.java
+++ day3Homework2/src/day3Homework2/Student.java
@@ -1,5 +1,7 @@
 package day3Homework2;
 
 public class Student extends User{
-	
+	private int id;
+	private String firstName;
+	private String lastName;
 }
